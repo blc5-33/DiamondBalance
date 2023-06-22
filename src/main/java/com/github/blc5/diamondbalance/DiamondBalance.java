18aedfc9570e82023c3d845b4cc3ce9645379f7f
mastersurvival5 <54222922+mastersurvival5@users.noreply.github.com>
2023-06-22 10:28:01 -0500

    Initial commit

---
+++ src/main/java/com/github/blc5/diamondbalance/DiamondBalance.java
@@ -0,0 +1,17 @@
+package com.github.blc5.diamondbalance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiamondBalance extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
